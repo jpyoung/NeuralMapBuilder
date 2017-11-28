4c9a989cb1059a0673ff76064a187aa666c9d8d7
Jack Young <jackp@vayoungs.us>
2017-11-28 15:04:54 -0500

    NMB-1: Added the project bin directory

---
+++ bin/lsof.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "lsof on port 3000 below:"
+lsof -i :3000
+
+
+# helpful vim commands
+# x - delete (cut) character
+# dw - delete (cut) the characters of the word from the cursor poisition to the start of the next word
+
+
---
+++ bin/start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+## Example of a broken script.
+
+echo "Echo Starting node server"
+npm start
